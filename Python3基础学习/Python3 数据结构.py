7a882fcee7861c0ea3f0a34f6930f46b692d3f8b
邱路 <qiulu0714@163.com>
2020-06-17 15:44:16 +0800

    学习python函数和数据结构

--- Python3基础学习/Python3 数据结构.py
+++ Python3基础学习/Python3 数据结构.py
@@ -76,4 +76,187 @@ print(queue.popleft())          # The first to arrive now leaves
 print(queue.popleft())          # The second to arrive now leaves
 
 print(queue)                    # Remaining queue in order of arrival
-deque(['Michael', 'Terry', 'Graham'])
+
+# ---------------------------------列表推导式
+# 列表推导式提供了从序列创建列表的简单途径。通常应用程序将一些操作应用于某个序列的每个元素，用其获得的结果作为生成新列表的元素，
+# 或者根据确定的判定条件创建子序列。
+# 每个列表推导式都在 for 之后跟一个表达式，然后有零到多个 for 或 if 子句。返回结果是一个根据表达从其后的 for 和 if 上下文环境中生成出来的列表。
+# 如果希望表达式推导出一个元组，就必须使用括号。
+# 这里我们将列表中每个数值乘三，获得一个新的列表：
+vec = [2, 4, 6]
+print([3*x for x in vec])
+
+print([[x, x**2] for x in vec])
+
+# 这里我们对序列里每一个元素逐个调用某方法：
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+print([weapon.strip() for weapon in freshfruit])
+
+# 我们可以用 if 子句作为过滤器：
+print([3*x for x in vec if x > 3])
+
+print([3*x for x in vec if x < 2])
+
+# 以下是一些关于循环和其它技巧的演示：
+vec1 = [2, 4, 6]
+vec2 = [4, 3, -9]
+print([x*y for x in vec1 for y in vec2])
+
+print([x+y for x in vec1 for y in vec2])
+
+print([vec1[i]*vec2[i] for i in range(len(vec1))])
+
+print(vec1[i]*vec2[j]for i in range(len(vec1)) for j in range(len(vec2)))
+
+for i in range(len(vec1)):
+    for j in range(len(vec2)):
+        t = vec1[i] * vec2[j]
+    print(t)
+
+# 列表推导式可以使用复杂表达式或嵌套函数：
+Rd = [str(round(355/113, i)) for i in range(1, 6)]
+print(Rd)
+
+# ---------------------------------嵌套列表解析
+# Python的列表还可以嵌套。
+# 以下实例展示了3X4的矩阵列表：
+matrix = [
+             [1, 2, 3, 4],
+             [5, 6, 7, 8],
+             [9, 10, 11, 12],
+]
+# 以下实例将3X4的矩阵列表转换为4X3列表：
+print([[row[i] for row in matrix] for i in range(4)])
+# 以下实例也可以使用以下方法来实现：
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+print(transposed)
+
+# 另外一种实现方法：
+transposed = []
+for i in range(4):
+# the following 3 lines implement the nested listcomp
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+        transposed.append(transposed_row)
+print(transposed)
+
+# ---------------------------del 语句
+# 使用 del 语句可以从一个列表中依索引而不是值来删除一个元素。这与使用 pop() 返回一个值不同。可以用 del 语句从列表中删除一个切割，
+# 或清空整个列表（我们以前介绍的方法是给该切割赋一个空列表）。例如：
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+del a[0]
+print(a)
+
+del a[2:4]
+print(a)
+
+del a[:]
+print(a)
+
+# 也可以用 del 删除实体变量：
+# b = [-1, 1, 66.25, 333, 333, 1234.5]
+# del b
+# print(b)
+
+
+# -------------------------------------------元组和序列
+# 元组由若干逗号分隔的值组成，例如：
+t = 12345, 54321, 'hello!'
+print(t[0])
+print(t)
+# Tuples may be nested:
+u = t, (1, 2, 3, 4, 5)
+print(u)
+# 如你所见，元组在输出时总是有括号的，以便于正确表达嵌套结构。在输入时可能有或没有括号， 不过括号通常是必须的（如果元组是更大的表达式的一部分）。
+# ---------------------------------------------集合
+# 集合是一个无序不重复元素的集。基本功能包括关系测试和消除重复元素。
+# 可以用大括号({})创建集合。注意：如果要创建一个空集合，你必须用 set() 而不是 {} ；后者创建一个空的字典，下一节我们会介绍这个数据结构。
+# 以下是一个简单的演示：
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)                      # 删除重复的
+print('orange' in basket)                 # 检测成员
+
+print('crabgrass' in basket)
+
+# 以下演示了两个集合的操作
+
+a = set('abracadabra')
+b = set('alacazam')
+print(a)                                  # a 中唯一的字母
+
+print(a - b)                             # 在 a 中的字母，但不在 b 中
+
+print(a | b)                              # 在 a 或 b 中的字母
+
+print(a & b)                              # 在 a 和 b 中都有的字母
+
+print(a ^ b)                              # 在 a 或 b 中的字母，但不同时在 a 和 b 中
+
+# 集合也支持推导式：
+
+a = {x for x in 'abracadabra' if x not in 'abc'}
+print(a)
+
+# -------------------------------------字典
+# 另一个非常有用的 Python 内建数据类型是字典。
+# 序列是以连续的整数为索引，与此不同的是，字典以关键字为索引，关键字可以是任意不可变类型，通常用字符串或数值。
+# 理解字典的最佳方式是把它看做无序的键=>值对集合。在同一个字典之内，关键字必须是互不相同。
+# 一对大括号创建一个空的字典：{}。
+# 这是一个字典运用的简单例子：
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+print(tel['jack'])
+
+del tel['sape']
+tel['jack'] = 4127
+print(tel)
+
+list(tel.keys())
+sorted(tel.keys())
+
+print('guido' in tel)
+
+print('jack' not in tel)
+
+# 构造函数 dict() 直接从键值对元组列表中构建字典。如果有固定的模式，列表推导式指定特定的键值对：
+abd = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(abd)
+# 此外，字典推导可以用来创建任意键和值的表达式词典：
+
+print({x: x**2 for x in (2, 4, 6)})
+
+# 如果关键字只是简单的字符串，使用关键字参数指定键值对有时候更方便：
+
+aen = dict(sape=4139, guido=4127, jack=4098)
+print(aen)
+
+
+# --------------------------------------遍历技巧
+# 在字典中遍历时，关键字和对应的值可以使用 items() 方法同时解读出来：
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k ,l in knights.items():
+    print(k, l)
+
+# 在序列中遍历时，索引位置和对应值可以使用 enumerate() 函数同时得到：
+listr = ['tic', 'tac', 'toe']
+for i in enumerate(listr):
+    print(i)
+
+# 同时遍历两个或更多的序列，可以使用 zip() 组合：
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}?  It is {1}.'.format(q, a))
+
+# 要反向遍历一个序列，首先指定这个序列，然后调用 reversed() 函数：
+for i in reversed(range(1, 10, 2)):
+    print(i)
+
+# 要按顺序遍历一个序列，使用 sorted() 函数返回一个已排序的序列，并不修改原值：
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(basket)):
+    print(f)
